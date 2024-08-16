5e6275a69af3c2565a47aaff2c9bdb6c0a8bba73
Jeremy Lumley <jlumley@tucows.com>
2024-08-16 19:08:40 -0400

    adding cli arguments and http server to fetch oauth token

---
+++ daily_pager/auth.py
@@ -0,0 +1,94 @@
+import http.server
+import socketserver
+import os
+import requests
+import threading
+import webbrowser
+
+from dataclasses import dataclass
+from urllib.parse import urlencode, urlparse, parse_qs
+
+# https://developer.deputy.com/deputy-docs/docs/using-a-permanent-token#url
+DEPUTY_AUTH_DOMAIN = "https://once.deputy.com"
+
+CLIENT_ID = os.environ.get("DEPUTY_CLIENT_ID")
+CLIENT_SECRET = os.environ.get("DEPUTY_CLIENT_SECRET")
+REDIRECT_URI = "http://localhost:8087"
+# REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
+AUTH_BASE_URL = f"{DEPUTY_AUTH_DOMAIN}/my/oauth/login"
+TOKEN_URL = f"{DEPUTY_AUTH_DOMAIN}/my/oauth/access_token"
+
+oauth_code = None
+
+
+@dataclass
+class DeputySession:
+    endpoint: str
+    access_token: str
+
+
+def get_authorization_url():
+    params = {
+        "client_id": CLIENT_ID,
+        "redirect_uri": REDIRECT_URI,
+        "response_type": "code",
+        "scope": "longlife_refresh_token",
+    }
+    url = f"{AUTH_BASE_URL}?{urlencode(params)}"
+    webbrowser.open_new_tab(url)
+
+
+def get_access_token(auth_code) -> DeputySession:
+    data = {
+        "grant_type": "authorization_code",
+        "scope": "longlife_refresh_token",
+        "code": auth_code,
+        "redirect_uri": REDIRECT_URI,
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+    }
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    response = requests.post(TOKEN_URL, headers=headers, data=data)
+    response.raise_for_status()
+    response_data = response.json()
+    return DeputySession(
+        access_token=response_data.get("access_token"),
+        endpoint=response_data.get("endpoint"),
+    )
+
+
+class OAuthHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        global oauth_code
+        # Parse the query string from the URL
+        parsed_url = urlparse(self.path)
+        query_params = parse_qs(parsed_url.query)
+
+        # Assuming the token is passed as "access_token" in the query string
+        if "code" in query_params:
+            oauth_code = query_params["code"][0]
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(
+                b"OAuth token received. You can close this window."
+            )  # noqa
+        else:
+            self.send_response(400)
+            self.end_headers()
+            self.wfile.write(b"Missing access_token in the query string.")
+
+
+def get_deputy_session():
+    get_authorization_url()
+    handler = OAuthHandler
+    port = 8087
+    with socketserver.TCPServer(("", port), handler) as httpd:
+        print(f"Waiting for OAuth token on http://localhost:{port}...")
+        httpd.handle_request()  # Wait for a single request
+
+        # Retrieve the token from the handler
+        httpd.server_close()
+
+    global oauth_code
+    return get_access_token(oauth_code)
--- run.py
+++ run.py
@@ -1,76 +1,158 @@
 #! usr/bin/env python3
+import argparse
+import json
+import re
 import requests
-import webbrowser
 
-from dataclasses import dataclass
-from urllib.parse import urlencode
+from datetime import datetime, date, timedelta
 
-# https://developer.deputy.com/deputy-docs/docs/using-a-permanent-token#url
-DEPUTY_AUTH_DOMAIN = "https://once.deputy.com"
-
-CLIENT_ID = "fbe26525c35c80a441191a9b57b43b0b2e2ecfdc"
-CLIENT_SECRET = "b3d7d6f32cc3d237bb415d645b6d42f7797c38e6"
-REDIRECT_URI = "http://localhost"
-# REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
-AUTH_BASE_URL = f"{DEPUTY_AUTH_DOMAIN}/my/oauth/login"
-TOKEN_URL = f"{DEPUTY_AUTH_DOMAIN}/my/oauth/access_token"
-
-print(DEPUTY_AUTH_DOMAIN)
+from daily_pager.auth import get_deputy_session, DeputySession
 
 
-@dataclass
-class DeputySession:
-    endpoint: str
-    access_token: str
+def validate_date(date_str: str):
+    pattern = re.compile(r"^\d{4}-\d{2}-\d{2}$")
+    if not pattern.match(date_str):
+        raise ValueError("Invalid date format. Expected {YYYY-MM-DD}")
+    return date_str
 
 
-def get_authorization_url():
-    params = {
-        "client_id": CLIENT_ID,
-        "redirect_uri": REDIRECT_URI,
-        "response_type": "code",
-        "scope": "longlife_refresh_token",
-    }
-    url = f"{AUTH_BASE_URL}?{urlencode(params)}"
-    webbrowser.open_new_tab(url)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    sub_parser = parser.add_subparsers(dest="cmd")
 
-
-def get_access_token(auth_code) -> DeputySession:
-    data = {
-        "grant_type": "authorization_code",
-        "scope": "longlife_refresh_token",
-        "code": auth_code,
-        "redirect_uri": REDIRECT_URI,
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET,
-    }
-    headers = {"Content-Type": "application/x-www-form-urlencoded"}
-    response = requests.post(TOKEN_URL, headers=headers, data=data)
-    response.raise_for_status()
-    response_data = response.json()
-    return DeputySession(
-        access_token=response_data.get("access_token"),
-        endpoint=response_data.get("endpoint"),
+    daily_pager = sub_parser.add_parser("pager", help="Add Daily Pager")
+    daily_pager.add_argument(
+        "--start-date",
+        "-s",
+        required=True,
+        help="Start date of the pager {YYYY-MM-DD}",
+        type=validate_date,
+    )
+    daily_pager.add_argument(
+        "--duration",
+        "-d",
+        required=True,
+        help="number of days carrying the pager",
+        type=int,
+    )
+    daily_pager.add_argument(
+        "--dry-run",
+        help="Do not submit the pager",
+        default=False,
+        action="store_true",
     )
 
+    return parser.parse_args()
 
-def get_employee_timesheets(session: DeputySession):
-    api_url = f"https://{session.endpoint}/api/v1/resource/Timesheet/QUERY"
+
+def get_employee(session: DeputySession):
+    api_url = f"https://{session.endpoint}/api/v1/my/employee/me"
     headers = {
         "Authorization": f"Bearer {session.access_token}",
-        "Content-Type": "application/x-www-form-urlencoded",
     }
-    query = {"search": {"s1": {"field": "Employee", "type": "eq", "data": 1}}}
-    response = requests.post(api_url, headers=headers, data=query, allow_redirects=True)
+    response = requests.get(api_url, headers=headers)
     print(response)
     print(response.content)
 
 
+# {
+#     "Id": 0,
+#     "Employee": 1337,
+#     "DateStart": "2024-08-05",
+#     "DateEnd": "2024-08-05",
+#     "LeaveRule": "31",
+#     "Comment": "na",
+#     "Notify": ["764"],
+#     "StartHour": 0,
+#     "StartMinute": 0,
+#     "EndHour": 1,
+#     "EndMinute": 0,
+#     "Status": 0,
+# }
+
+# notify Danny = 764
+
+
+def add_employee_leave(
+    session: DeputySession,
+    employee_id: int,
+    date_str: str,
+    notify: str,
+    comment: str,
+):
+    api_url = f"https://{session.endpoint}/api/v1/my/leave"
+    headers = {
+        "Authorization": f"Bearer {session.access_token}",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-Requested-With": "XMLHttpRequest",
+    }
+    payload = {
+        "Id": 0,
+        "Employee": 1337,
+        "DateStart": date_str,
+        "DateEnd": date_str,
+        "LeaveRule": "31",
+        "Comment": comment,
+        "Notify": [notify],
+        "StartHour": 0,
+        "StartMinute": 0,
+        "EndHour": 1,
+        "EndMinute": 0,
+        "Status": 0,
+    }
+    response = requests.post(
+        api_url, headers=headers, data=json.dumps(payload), allow_redirects=True  # noqa
+    )
+
+    response.raise_for_status()
+    print(f"Leave added successfully for {date_str}")
+
+
+def submit_daily_pager(
+    session: DeputySession,
+    employee_id: int,
+    start_date: str,
+    duration: int,
+    notify: str,
+    dry_run: bool,
+):
+    """
+    Submit daily pager for the given
+    employee for the specified duration
+    """
+    date_obj = datetime.strptime(start_date, "%Y-%m-%d")
+    for i in range(duration):
+        date_str = date_obj.strftime("%Y-%m-%d")
+        print(f"Adding Leave for {date_str}")
+        if not dry_run:
+            add_employee_leave(
+                session=session,
+                employee_id=employee_id,
+                date_str=date_str,
+                notify=notify,
+                comment="Daily Pager",
+            )
+        date_obj += timedelta(days=1)
+
+
 def main():
-    get_authorization_url()
-    auth_code = input("Enter the authorization code: ")
-    access_token = get_access_token(auth_code)
-    get_employee_timesheets(access_token)
+    parser = parse_args()
+    if parser.dry_run:
+        access_token = "dummy"
+        print("Dry Run Mode")
+    else:
+        access_token = get_deputy_session()
+        print("Access Token Successfully Obtained")
+
+    if parser.cmd == "pager":
+        submit_daily_pager(
+            access_token,
+            employee_id=1337,
+            start_date=parser.start_date,
+            duration=parser.duration,
+            notify="764",
+            dry_run=parser.dry_run,
+        )
 
 
 if __name__ == "__main__":
